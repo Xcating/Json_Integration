a9ded944ceddb263f7dd19d7258b3e31d4a49f2f
Xcating <927168329@qq.com>
2023-09-06 20:07:44 +0800

    fix

--- Update.bat
+++ Update.bat
@@ -1,15 +1,15 @@
 @echo off
 :begin
-echo 打开更新地址还是打开Github仓库(a/b)
+echo Open update address or open Github warehouse (a/b)
 set /p abc=
 if "%abc%"=="a" goto a
 if "%abc%"=="b" goto b
-echo 你输入了个啥玩意啊，就小写的a和b
+echo Only can send a / b
 pause
 goto begin
 :a
-start https://github.com/Xcating/Json_Integration/archive/refs/heads/main.zip
+start https://github.com/Xcating/Json_Integration/archive/refs/heads/dev.zip
 pause
 :b
-start https://github.com/Xcating/Json_Integration
+start https://github.com/Xcating/Json_Integration/tree/dev
 pause
